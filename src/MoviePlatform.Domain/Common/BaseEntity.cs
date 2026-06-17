6c5eba3f18d514779ce04b93953f133338bdd8d2
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-17 16:43:54 +0400

    chore(domain): implement BaseEntity class

---
+++ src/MoviePlatform.Domain/Common/BaseEntity.cs
@@ -0,0 +1,52 @@
+namespace MoviePlatform.Domain.Common;
+
+public abstract class BaseEntity<TId> : IEquatable<BaseEntity<TId>> where TId : struct
+{
+	public TId Id { get; init; }
+
+	protected BaseEntity(TId id)
+    {
+        Id = id;
+    }
+
+	public bool Equals(BaseEntity<TId>? other)
+	{
+		if (other is null)
+		{
+			return false;
+		}
+
+        if (ReferenceEquals(this, other))
+		{
+			return true;
+		}
+
+		if (other.GetType() != GetType())
+		{
+			return false;
+		}
+
+		return Id.Equals(other.Id);
+	}
+
+	public override bool Equals(object? obj) => obj is BaseEntity<TId> other && Equals(other);
+
+	public override int GetHashCode() => HashCode.Combine(GetType(), Id);
+
+	public static bool operator ==(BaseEntity<TId>? left, BaseEntity<TId>? right)
+	{
+		if (left is null && right is null)
+		{
+			return true;
+		}
+
+		if (left is null || right is null)
+		{
+			return false;
+		}
+
+		return left.Equals(right);
+	}
+
+	public static bool operator !=(BaseEntity<TId>? left, BaseEntity<TId>? right) => !(left == right);
+}
