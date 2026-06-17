2bc5a88c8de28510724bede90d081b6d7f346307
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-17 17:04:37 +0400

    chore(domain): implement PagedList class

---
+++ src/MoviePlatform.Domain/Common/PagedList.cs
@@ -0,0 +1,25 @@
+namespace MoviePlatform.Domain.Common;
+
+public sealed class PagedList<T>
+{
+    public List<T> Items { get; }
+    public int Page { get; }
+    public int PageSize { get; }
+    public int TotalCount { get; }
+    public int TotalPages => (int) Math.Ceiling((double) TotalCount / PageSize);
+    public bool HasNextPage => Page < TotalPages;
+    public bool HasPreviousPage => Page > 1;
+
+	private PagedList(List<T> items, int page, int pageSize, int totalCount)
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
+    public static PagedList<T> Create(List<T> items, int page, int pageSize, int totalCount)
+    {
+        return new(items, page, pageSize, totalCount);
+    }
+}
