0257d1d193a65627bb2cf3a19292c09864188d5f
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-18 22:35:33 +0400

    chore(infra): add comfiguration details for DbContext

--- src/MoviePlatform.Infrastructure/DependencyInjection.cs
+++ src/MoviePlatform.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,12 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using MoviePlatform.Application.Common.Authentication;
 using MoviePlatform.Infrastructure.Authentication;
+using MoviePlatform.Infrastructure.Persistence;
 
 namespace MoviePlatform.Infrastructure;
 
@@ -12,7 +14,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton<IJwtProvider, JwtProvider>();
+		services.AddSingleton<IJwtProvider, JwtProvider>();
 		services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>();
 
 		IConfigurationSection jwtConfigurationSection = configuration.GetSection("JwtSettings");
@@ -37,6 +39,10 @@ public static class DependencyInjection
 			});
 
 		services.AddAuthorization();
+
+		services.AddDbContext<MoviePlatformDbContext>(
+			options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")
+					?? throw new InvalidOperationException("Database connection string is missing or invalid.")));
 
 		return services;
     }
