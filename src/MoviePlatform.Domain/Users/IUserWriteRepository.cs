7cadd10bcced0d2ebc8e032b85c25f962efbae8c
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-18 15:53:03 +0400

    chore(domain): add IUserWriteRepository interface

---
+++ src/MoviePlatform.Domain/Users/IUserWriteRepository.cs
@@ -0,0 +1,11 @@
+using MoviePlatform.Domain.Users.ValueObjects;
+
+namespace MoviePlatform.Domain.Users;
+
+public interface IUserWriteRepository
+{
+	Task<User?> GetByIdAsync(UserId userId, CancellationToken cancellationToken = default);
+    Task<bool> IsEmailUniqueAsync(Email email, CancellationToken cancellationToken = default);
+    void Add(User user);
+    void Update(User user);
+}
